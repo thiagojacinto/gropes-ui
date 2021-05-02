49a8e272452e402e09d02e18ef53373b0646e790
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-05-02 00:39:53 -0300

    feat: ajustes para usar componente da lib surveyJS

--- iodev-ui/src/app/app.component.ts
+++ iodev-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import surveyData from '../assets/survey-data.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ioDev';
+  data: string;
+
+  constructor() {
+    this.data = JSON.stringify(surveyData);
+  }
 }
