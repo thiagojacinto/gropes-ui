1627e6e2248e67eec2b8c307a02a3595fad979f1
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-05-29 15:49:46 -0300

    feat: listar tecnologias das experiências registradas em uma timeline

---
+++ iodev-ui/src/app/dashboard/lista-items/item/item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  @Input() item: any
+
+  constructor() {
+    console.log(`[INFO] <item> ... Received item data: ${JSON.stringify(this.item)}`);
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ iodev-ui/src/app/dashboard/lista-items/lista-items.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaItemsComponent } from './lista-items.component';
+
+describe('ListaItemsComponent', () => {
+  let component: ListaItemsComponent;
+  let fixture: ComponentFixture<ListaItemsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaItemsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListaItemsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
