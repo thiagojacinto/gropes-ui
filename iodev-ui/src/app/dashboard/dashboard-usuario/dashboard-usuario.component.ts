5525ef547475d3d2687d2f20d14494056621b381
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-05-29 15:49:14 -0300

    feat: dashboard com único gráfico de SCORE

--- iodev-ui/src/app/dashboard/dashboard-usuario/dashboard-usuario.component.ts
+++ iodev-ui/src/app/dashboard/dashboard-usuario/dashboard-usuario.component.ts
@@ -4,34 +4,40 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-dashboard-usuario',
   templateUrl: './dashboard-usuario.component.html',
-  styleUrls: ['./dashboard-usuario.component.css']
+  styleUrls: ['./dashboard-usuario.component.css'],
 })
 export class DashboardUsuarioComponent implements OnInit {
-
   dados: any;
+  avisoDisponibilidade = false;
 
-  constructor(private activRoute: ActivatedRoute) { }
+  constructor(private activRoute: ActivatedRoute) {}
 
   maisInfo(): void {
-    console.log('Mais info sobre !');
+    console.log('[INFO] CLICK at Mais info sobre!');
+    this.funcionalidadeIndisponivel()
+  }
+  editarPerfil(): void {
+    console.log('[INFO] CLICK at Editar perfil!');
+    this.funcionalidadeIndisponivel()
   }
 
   ngOnInit(): void {
     this.activRoute.data.subscribe(
       () => {
-        this.dados = this.activRoute.snapshot.data['dados']
+        this.dados = this.activRoute.snapshot.data['dados'];
         console.log(this.dados);
       },
       (error) => console.error(error)
-    )
+    );
   }
 
   get nomeUsuario(): string {
     try {
-      const nomes = this.dados?.tecnologiasUsuario.map((tecUser: any) => tecUser.usuario.nome);
-      const unique = [... new Set(nomes)];
+      const nomes = this.dados?.tecnologiasUsuario.map(
+        (tecUser: any) => tecUser.usuario.nome
+      );
+      const unique = [...new Set(nomes)];
       return unique[0] as string;
-      
     } catch (error) {
       console.error(error);
       return '<Usuario>';
@@ -40,13 +46,17 @@ export class DashboardUsuarioComponent implements OnInit {
 
   get scoreUsuario(): number {
     try {
-      const scores = this.dados?.tecnologiasUsuario.map((tecUser: any) => tecUser.usuario.score);
+      const scores = this.dados?.tecnologiasUsuario.map(
+        (tecUser: any) => tecUser.usuario.score
+      );
       return scores[0] ? scores[0] : 0;
-      
     } catch (error) {
       console.error(error);
       return 0;
     }
   }
 
+  funcionalidadeIndisponivel() {
+    this.avisoDisponibilidade = true;
+  }
 }
