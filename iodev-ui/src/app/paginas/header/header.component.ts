c91729f3a7a1bd0f7802bdb2ed9f88d02a97f5f1
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-05-31 23:26:35 -0300

    feat: config inicial do header

---
+++ iodev-ui/src/app/paginas/header/header.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MenuItem, PrimeIcons } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  menuItens!: MenuItem[];
+  avisoDisponibilidade = false;
+
+  funcionalidadeIndisponivel() {
+    this.avisoDisponibilidade = true;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.menuItens = [
+      {
+        label: 'ioDev',
+      },
+      {
+        label: 'Registre-se',
+        routerLink: ['/registro/usuario']
+      },
+      {
+        label: 'Análise',
+        icon: PrimeIcons.CHART_LINE,
+        routerLink: ['/dashboard/5'],
+      },
+      {
+        label: 'Sobre',
+        icon: PrimeIcons.BOOK,
+      },
+      {
+        label: 'Blog',
+        command: (evento: any) => this.funcionalidadeIndisponivel(),
+      },
+    ];
+  }
+}
